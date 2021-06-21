2773c1c7aac04a92a59dae249a1218eea8197e3e
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-21 15:48:09 +0200

    stampa hello word

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+
+
+    $data = 
+    [
+      'title'=> 'hello word',
+    ];
+
+
+    return view('home', $data);
+    return view('home', $data);
 });
+
+   Route::get('/abaut', function () {
+    return view('abaut');
+   });    
